5be4b0ad828223b33caa445a0c54e564d4fc405e
0xF4D3C0D3 <dongho971220@gmail.com>
2020-11-19 10:54:55 +0900

    add database helper functions

---
+++ app/dummy_data/db.py
@@ -0,0 +1,24 @@
+import itertools
+
+import sqlalchemy
+import psycopg2
+
+def execute(cmd):
+    with psycopg2.connect(user='postgres', database='kw_db', host='localhost') as conn:
+        with conn.cursor() as cur:
+            cur.execute(cmd)
+
+def fetch(cmd):
+    with psycopg2.connect(user='postgres', database='kw_db', host='localhost') as conn:
+        with conn.cursor() as cur:
+            cur.execute(cmd)
+            result = cur.fetchall()
+            if len(result[0]) == 1:
+                return list(itertools.chain.from_iterable(result))
+            else:
+                return result
+
+def insert(df, table_name):
+    engine = sqlalchemy.create_engine('postgresql+psycopg2://postgres@localhost/kw_db')
+    with engine.begin() as conn:
+        df.to_sql(table_name, conn, if_exists='append', method='multi', chunksize=5000, index=False)
