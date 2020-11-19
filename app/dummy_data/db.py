76a6d42e049851d4eca8f9f6a68dab73e7e1654c
0xF4D3C0D3 <dongho971220@gmail.com>
2020-11-19 17:07:40 +0900

    use socket instead of http

--- app/dummy_data/db.py
+++ app/dummy_data/db.py
@@ -4,12 +4,12 @@ import sqlalchemy
 import psycopg2
 
 def execute(cmd):
-    with psycopg2.connect(user='postgres', database='kw_db', host='localhost') as conn:
+    with psycopg2.connect(user='postgres', database='kw_db') as conn:
         with conn.cursor() as cur:
             cur.execute(cmd)
 
 def fetch(cmd):
-    with psycopg2.connect(user='postgres', database='kw_db', host='localhost') as conn:
+    with psycopg2.connect(user='postgres', database='kw_db') as conn:
         with conn.cursor() as cur:
             cur.execute(cmd)
             result = cur.fetchall()
@@ -19,6 +19,6 @@ def fetch(cmd):
                 return result
 
 def insert(df, table_name):
-    engine = sqlalchemy.create_engine('postgresql+psycopg2://postgres@localhost/kw_db')
+    engine = sqlalchemy.create_engine('postgresql+psycopg2://postgres@/kw_db')
     with engine.begin() as conn:
         df.to_sql(table_name, conn, if_exists='append', method='multi', chunksize=5000, index=False)
