8c6d07769fa16597eac49b7d4e479513711dc6f3
0xF4D3C0D3 <dongho971220@gmail.com>
2020-11-23 04:52:25 +0900

    fix typo

--- apps/api/app.py
+++ apps/api/app.py
@@ -5,8 +5,8 @@ from resources.post import api as post_api
 
 app = Flask(__name__)
 api = Api(app, version='1.0', title='KW SIS API',
-    description='A Student Inforamtion System API for KwangWoon univ'
-)
+          description='A Student Information System API for KwangWoon univ'
+          )
 
 api.add_namespace(post_api)
 
