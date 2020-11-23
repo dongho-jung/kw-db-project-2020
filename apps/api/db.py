d5d78fb9c58f06b526b40e968d412df8c27b0745
0xF4D3C0D3 <dongho971220@gmail.com>
2020-11-24 04:31:32 +0900

    refactor api

--- apps/api/db.py
+++ apps/api/db.py
@@ -1,13 +1,15 @@
 import itertools
 import os
 
-import sqlalchemy
 import psycopg2
+import sqlalchemy
+
 
 def execute(cmd):
     with psycopg2.connect(user='postgres', database='kw_db', host=os.environ['KW_DB_HOST']) as conn:
         with conn.cursor() as cur:
             cur.execute(cmd)
+
 
 def fetch(cmd):
     with psycopg2.connect(user='postgres', database='kw_db', host=os.environ['KW_DB_HOST']) as conn:
@@ -15,9 +17,11 @@ def fetch(cmd):
             cur.execute(cmd)
             result = cur.fetchall()
             if len(result) > 0 and len(result[0]) == 1:
-                return list(itertools.chain.from_iterable(result))
-            else:
-                return result
+                result = list(itertools.chain.from_iterable(result))
+            if len(result) == 1:
+                return result[0]
+            return result
+
 
 def insert(df, table_name):
     engine = sqlalchemy.create_engine('postgresql+psycopg2://postgres@/kw_db', host=os.environ['KW_DB_HOST'])
@@ -29,6 +33,6 @@ def join_params_for_where(predicates, delim='AND'):
     res = ''
     for pred in predicates:
         res += f'{pred} {delim} '
-    
+
     res = res.rsplit(delim, 1)[0]
     return res
