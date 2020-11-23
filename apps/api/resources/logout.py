9264ef781943a09bad34864dafb2559e07118ae2
0xF4D3C0D3 <dongho971220@gmail.com>
2020-11-24 04:25:39 +0900

    init login related resources

---
+++ apps/api/resources/logout.py
@@ -0,0 +1,11 @@
+from flask_login import login_required, logout_user
+from flask_restx import Namespace, Resource
+
+api = Namespace('logout', description='Logout related operations')
+
+
+@api.route('/')
+class Logout(Resource):
+    @login_required
+    def get(self):
+        logout_user()
