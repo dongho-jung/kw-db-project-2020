643cb8ca6e035cf04d2bcf5f909363ca08c4c653
0xF4D3C0D3 <dongho971220@gmail.com>
2020-11-26 05:58:00 +0900

    add scholarship api

--- apps/api/app.py
+++ apps/api/app.py
@@ -7,6 +7,7 @@ from kw_sis_session import login_manager
 from resources.login import api as login_api
 from resources.logout import api as logout_api
 from resources.post import api as post_api
+from resources.scholarship import api as scholarship_api
 
 app = Flask(__name__)
 app.secret_key = os.environ['KW_APP_SECRET']
@@ -20,6 +21,7 @@ api = Api(app, version='1.0', title='KW SIS API',
 api.add_namespace(post_api)
 api.add_namespace(login_api)
 api.add_namespace(logout_api)
+api.add_namespace(scholarship_api)
 
 if __name__ == '__main__':
     app.run(debug=True)
