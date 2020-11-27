9d865c62c37f9cbeeb69ecc57b0e22600028e9ea
HoSungChoo <57245883+HoSungChoo@users.noreply.github.com>
2020-11-28 01:18:48 +0900

    Update List2.vue

--- src2/components/board/List2.vue
+++ src2/components/board/List2.vue
@@ -2,20 +2,28 @@
 	<div>
 		<h2>게시판 리스트</h2>
 		<a href="javascript:;" @click="getList">GET 방식 데이터 접근</a>
-	</div>
+	
+        <div class="box" v-for="(user, idx) in users" :key="idx">
+            <h2>{{user.first_name}}</h2>
+            <img :src="`${user.avatar}`" alt />
+        </div>
+        <Paging :totalPage="totalPage" />
+    </div>
+
 </template>
 
 <script>
 export default {
 	methods:{
 		getList() {
-			this.$axios.get("http://localhost:3306/api/board")
-			.then((res)=>{
-				console.log(res);
-			})
-			.then((err)=>{
-				console.log(err);
-			})
+			this.$axios.get("https://reqres.in/api/users?page=2")
+			.then(res => {
+                this.users = res.data.data;
+                this.totalPage = res.data.total_pages;
+            })
+            .catch(err => {
+                console.log(err);
+            });
 		}
 	}
 }
