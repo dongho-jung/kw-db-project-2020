5ff982235b404503a80145d7aa8932e026cac414
leekatme <leesk212@gmail.com>
2020-12-01 01:31:06 +0900

    20/12/01 01:26 업데이

--- Frontend/src/components/login/Login.vue
+++ Frontend/src/components/login/Login.vue
@@ -39,61 +39,6 @@
     </form>
   </div>
 </template>
-<!--<template>-->
-<!--  <div class="Login_part">-->
-<!--    <div class="Bbox1">-->
-<!--      <img alt="DB1" src="@/assets/kw01.jpg" height = "200" width="320">-->
-<!--      <h1>DB MANMAN<br>Time Planner</h1>-->
-<!--    </div>-->
-
-<!--    <div class="Bbox2">-->
-<!--      <div class="BPad">-->
-<!--      </div>-->
-<!--      <div class="Bbox21">-->
-<!--        <font size="2em" face="bold">ID : </font>-->
-<!--      </div>-->
-<!--      <div class="Bbox22">-->
-<!--       <input id="username" type="text" v-model="username" size=50 style = "text-align:center;"/>-->
-<!--      </div>-->
-<!--      <div class="Bbox23">&lt;!&ndash; >버튼< &ndash;&gt;-->
-<!--        <button id = "New Account" style = "width:80pt" v-on:click="btn_NewAccount">New Account</button>-->
-<!--        &lt;!&ndash; ><input type="button" value="New Account" size=70 style = "width:80pt;height:16pt;text-align:center;">< &ndash;&gt;-->
-<!--      </div>-->
-<!--      <div class="BPad">-->
-<!--      </div>-->
-<!--    </div>-->
-
-<!--    <div class="Bbox3">-->
-<!--      <div class="BPad">-->
-<!--      </div>-->
-<!--      <div class="Bbox31">-->
-<!--        <font size="2em" face="bold">PW : </font>-->
-<!--      </div>-->
-<!--      <div class="Bbox32">-->
-<!--        <input id="password" type="psassword" v-model="password" name="ID" size=50 style = "text-align:center;">-->
-<!--      </div>-->
-<!--      <div class="Bbox33">&lt;!&ndash; >버튼< &ndash;&gt;-->
-<!--        <button id = "Find PW" style = "width:80pt" v-on:click="btn_FindPW">Find PW</button>-->
-<!--        &lt;!&ndash; ><input type="button" value="Find PW" size=70 style = "width:80pt;height:16pt;text-align:center;">< &ndash;&gt;-->
-<!--      </div>-->
-<!--      <div class="BPad">-->
-<!--      </div>-->
-<!--    </div>-->
-
-<!--    <div class="Bbox4">-->
-<!--      <div class="BPad">-->
-<!--      </div>-->
-<!--      <div class="Bbox41">&lt;!&ndash; >버튼< &ndash;&gt;-->
-<!--        <input type="button" v-on:click="btn_Login" value="Log In" size=50 style = "width:160pt;height:47pt;text-align:center;">-->
-<!--      </div>-->
-<!--      <div class="Bbox42">&lt;!&ndash; >버튼< &ndash;&gt;-->
-<!--        <a href="http://localhost:3000/NewAccount"><img src="../../assets/kakao.png"></a>-->
-<!--      </div>-->
-<!--      <div class="BPad">-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </div>-->
-<!--</template>-->
 
 <script>
 
