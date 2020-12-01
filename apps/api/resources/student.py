b9c7cc1b1481cfe095f80432c473a59021fa4596
0xF4D3C0D3 <dongho971220@gmail.com>
2020-12-01 11:47:53 +0900

    remove unnecessary parameters for GET /student
    Signed-off-by: 0xF4D3C0D3 <dongho971220@gmail.com>

--- apps/api/resources/student.py
+++ apps/api/resources/student.py
@@ -5,13 +5,6 @@ import db
 
 api = Namespace('student', description='Student related operations')
 
-# 학생 조회
-student_get_parser = reqparse.RequestParser()
-student_get_parser.add_argument('student_id', type=str)
-student_get_parser.add_argument('name', type=str)
-student_get_parser.add_argument('phone', type=str)
-student_get_parser.add_argument('email', type=str)
-
 # 학생갱신
 student_put_parser = reqparse.RequestParser()
 student_put_parser.add_argument('name', type=str)
@@ -25,20 +18,13 @@ student_put_parser.add_argument('process_id', type=str)
 
 @api.route('')
 class Student(Resource):
-    @api.expect(student_get_parser)
     @login_required
     def get(self):
-        params = student_get_parser.parse_args()
-
-        predicates = []
-        if params['student_id']: predicates += [f"student_id = '%{params['student_id']}%'"]
-        if params['name']: predicates += [f"name LIKE '%{params['name']}%'"]
-        if params['phone']: predicates += [f"phone = '%{params['phone']}%'"]
-        if params['email']: predicates += [f"email = '%{params['email']}%'"]
+        student_id = current_user.id
 
         rows = db.fetch(f'''SELECT id, name, major_id, year, semester, phone, email, professor_id
                             FROM student
-                            WHERE {db.join_params_for_where(predicates)}
+                            WHERE id = {student_id}
                ''')
         cols = ['id', 'name', 'major_id', 'year', 'semester', 'phone', 'email', 'professor_id']
 
