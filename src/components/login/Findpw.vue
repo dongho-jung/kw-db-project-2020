319d4ab546bfc0b7d32d2fb22dcda370880604cf
whitejh <kei02012@naver.com>
2020-12-01 14:52:04 +0900

    Findpwcss수정

--- src/components/login/Findpw.vue
+++ src/components/login/Findpw.vue
@@ -1,27 +1,138 @@
 <template>
-  <div class="box2">
-    <a href="http://localhost:8080/Findpw">
+  <div class="simpleLogin">
+    <form>
+    <a href="http://localhost:3000/Findpw">
       <img src="../../assets/password.png" height = "200" width="300">
+      <legend>
       <h1>Find Password</h1>
+      </legend>
     </a>
-      <tr>
-        <div class="box21">
-        Student ID : <input type="text" name="student_id" size = 48 style = "text-align:center;"><br><br><br>
-        </div>
-        <div class="box21">
-        Name : <input type="text" name="name" size = 53 style = "text-align:center;"><br><br><br>
-        </div>
-        Phone : <input type="text" name="phone" size = 53 style = "text-align:center;"><br><br><br>
-        Email : <input type="text" name="email" size = 38 style = "text-align:center;">
-                <input type="button" value="중복체크" size = 50 style = "width:80pt;height:16pt;text-align:center;"><br><br><br>
-                <input type="button" value="OK" size=70 style = "width:100pt;height:20pt;text-align:center;">
-                <input type="button" value="Cancel" size=70 style = "width:100pt;height:20pt;text-align:center;">
-      </tr>
+    <tr>
+      <div class="box21">
+      * Student ID : <input v-model="student_id" type="text" name="student_id" size = 48 style = "text-align:center;"><br><br>
+      </div>
+      <div class="box21">
+      * Name : <input v-model="Name" type="text" name="name" size = 53 style = "text-align:center;"><br><br>
+      </div>
+        Phone : <input type="text" name="phone" size = 55 style = "text-align:center;"><br><br>
+      * Email : <input v-model="email" type="text" name="email" size = 54 style = "text-align:center;"><br><br>
+
+      <div class="Bbox41" style="padding:10px;">
+        <div class="BPad"></div>
+      <input type="button" v-on:click="OK" value="OK" size=70 style = "width:100pt;height:20pt;text-align:center;">
+       <div class="BPad"></div>
+      <input type="button" v-on:click="Cancel" value="Cancel" size=70 style = "width:100pt;height:20pt;text-align:center;">
+       <div class="BPad"></div>
+      </div>
+
+    </tr>
+    </form>
   </div>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      student_id:'',
+      Name:'',
+      email:''
+    }
+  },
+  methods: {
+    Check_email: function() {
+      if(this.kakao_email == ''){
+        alert('Empty Email!!')
+      }
+      let bodyFormdata = new FormData();
+      bodyFormdata.append('student_id',this.student_id)
+      axios({
+        method: 'get',
+        baseURL: 'http://localhost:5000',
+        url: '/student',
+        data: bodyFormdata,
+        headers: {'Content-Type': 'multipart/form-data' }
+      })
+          .then(res=>{ //못찾으면 error 출력하도록 유도
+            console.log(res);
+            alert('Already Presented Email')
+          }), async function (error){
+        console.log(error);
+        alert('You can use it')
+        this.$cookies.set('CheckDuplicatedEmail')
+      }
+    },
+    OK() {
+      if (this.email=='') {
+        alert('Success to Make New Account')
+        this.$router.push('/login')
+      } else {
+        alert('')
+      }
+    },
+    Cancel() {
+      alert('Go to Login Page')
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+.simpleLogin form {
+  width: 600px;
+  height: 500px;
+  margin: auto;
+  background: #fefefd;
+  box-shadow: 0 10px 10px #222;
+}
+.simpleLogin form fieldset {
+  display : grid;
+  border: 0 none;
+  margin: 0;
+  padding: 50px;
+}
+.simpleLogin form fieldset input,
+.simpleLogin form legend {
+  font-family: Open Sans;
+  font-size: 15px;
+}
+.simpleLogin form legend {
+  background-color: #8fc400;
+  border-top: 0 none;
+  color: white;
+  display: table-cell;
+  padding: 10px 20px;
+  width: auto;
+}
+.simpleLogin form fieldset input {
+  width: 90%;
+  margin: 10px 0;
+  padding: 10px 5%;
+  border: thin #8fc400 solid;
+}
+.simpleLogin input[type="submit"] {
+  width: 100px;
+  float: right;
+  background: #8fc400;
+  color: white;
+  transition: 0.2s;
+  border: 0;
+  cursor: pointer;
+}
+.simpleLogin input[type="submit"]:focus,
+.simpleLogin input[type="submit"]:hover,
+.simpleLogin input[type="submit"]:active {
+  padding: 10px 5%;
+  background: #B3E226;
+  outline: none;
+}
+
+
 .button {
   color: black;
   padding: 7px 60px;
@@ -31,38 +142,31 @@
   cursor: pointer;
   margin: 4px 2px;
 }
-
-
 .box2{
   padding:9px;
   display: grid;
   grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 9fr;
   grid-gap: 0px;
 }
-
 .box2n{
   padding:9px;
   display: grid;
   grid-template-columns: 2fr 1fr;
   grid-gap: 1px;
-  border: 1px solid; 
+  border: 1px solid;
 }
-
-
 #header, #nav, #section, #footer { text-align:center; }
 #header, #footer { line-height:100px; }
 #nav, #section { line-height:240px; }
-
 .box4{
   display: grid;
   grid-template-columns: 10fr 3fr 3fr 10fr;
 }
-
 .box4n{
   padding:9px;
   display: grid;
   grid-template-columns: 2fr 1fr;
   grid-gap: 1px;
-  border: 1px solid; 
+  border: 1px solid;
 }
 </style>
--- src/components/login/NewAccount.vue
+++ src/components/login/NewAccount.vue
@@ -1,34 +1,88 @@
 <template>
-  <div class="box2">
-    <a href="http://localhost:8080/NewAccount">
+  <div class="simpleLogin">
+    <form>
+    <a href="http://localhost:3000/NewAccount">
       <img src="../../assets/time.png" height = "200" width="300">
       <h1>New Account</h1>
+      <input type="text" name="student_id" size = 48 style = "width:200pt;height:200pt;text-align:center;"><br><br><br>
     </a>
-      <tr>
-        <div class="box21">
-        Student ID : <input type="text" name="student_id" size = 48 style = "text-align:center;"><br><br><br>
-        </div>
-        <div class="box21">
-        Name : <input type="text" name="name" size = 53 style = "text-align:center;"><br><br><br>
-        </div>
-        Phone : <input type="text" name="phone" size = 53 style = "text-align:center;"><br><br><br>
-        Email : <input type="text" name="email" size = 38 style = "text-align:center;">
-                <input type="button" value="중복체크" size = 50 style = "width:80pt;height:16pt;text-align:center;"><br><br><br>
-    
-        PW :    <input type="text" name="PW" size = 56 maxlength=10><br><br><br>  
-        <input type="button" value="OK" size=70 style = "width:100pt;height:20pt;text-align:center;">
-        <input type="button" value="Cancel" size=70 style = "width:100pt;height:20pt;text-align:center;">
-         
-        <div class="Apad"><!-- >버튼< --></div>
-        <div class="Bbox42"><!-- >버튼< -->
-        <a href="http://localhost:8080/NewAccount"><img src="../../assets/kakaoNewAccount.png"></a>
-        </div>
-      </tr>
+    <tr>
+      <img v-bind:src="kakao_profile_image">
+
+      <div class="box21">
+        * Student ID : <input v-model="student_id" type="text" name="student_id" size = 48 style = "text-align:center;"><br><br><br>
+      </div>
+
+      <div class="box21">
+        * Name : <input v-model="kakao_name" type="text" name="name" size = 53 style = "text-align:center;"><br><br><br>
+      </div>
+      Phone : <input type="text" name="phone" size = 53 style = "text-align:center;"><br><br><br>
+      Gender : <input v-model="kakao_gender" type="text" name="gender" size = 53 style = "text-align:center;"><br><br><br>
+      * Email : <input v-model="kakao_email" type="text" name="email" size = 38 style = "text-align:center;">
+      <input v-on:click="Check_email" type="button" value="중복체크" size = 50 style = "width:80pt;height:16pt;text-align:center;"><br><br><br>
+      Birth(Month,Date) : <input type="text" v-model="kakao_birth" name="birth" size = 53 style = "text-align:center;"><br><br><br>
+      * PW :    <input type="text" name="PW" size = 56 maxlength=10><br><br><br>
+      <div class="Bbox42"><!-- >버튼< -->
+        <p v-on:click="KakaoLogin" ><a href="https://kauth.kakao.com/oauth/authorize?client_id=8d323fc0c13720cda59983912f875316&redirect_uri=http://localhost:5000/oauth&response_type=code"><img src="../../assets/kakaoNewAccount.png"></a></p>
+      </div>
+      <input type="button" v-on:click="OK" value="OK" size=70 style = "width:100pt;height:20pt;text-align:center;">
+      <input type="button" v-on:click="Cancel" value="Cancel" size=70 style = "width:100pt;height:20pt;text-align:center;">
+      <div class="Apad"><!-- >버튼< --></div>
+
+    </tr>
+    </form>
   </div>
 </template>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+.simpleLogin form {
+  width: 800px;
+  height: 800px;
+  margin: auto;
+  background: #fefefd;
+  box-shadow: 0 10px 10px #222;
+}
+.simpleLogin form fieldset {
+  display : grid;
+  border: 0 none;
+  margin: 0;
+  padding: 50px;
+}
+.simpleLogin form fieldset input,
+.simpleLogin form legend {
+  font-family: Open Sans;
+  font-size: 15px;
+}
+.simpleLogin form legend {
+  background-color: #8fc400;
+  border-top: 0 none;
+  color: white;
+  display: table-cell;
+  padding: 10px 20px;
+  width: auto;
+}
+.simpleLogin form fieldset input {
+  width: 90%;
+  margin: 10px 0;
+  padding: 10px 5%;
+  border: thin #8fc400 solid;
+}
+.simpleLogin input[type="submit"] {
+  width: 100px;
+  float: right;
+  background: #8fc400;
+  color: white;
+  transition: 0.2s;
+  border: 0;
+  cursor: pointer;
+}
+.simpleLogin input[type="submit"]:focus,
+.simpleLogin input[type="submit"]:hover,
+.simpleLogin input[type="submit"]:active {
+  padding: 10px 5%;
+  background: #B3E226;
+  outline: none;
+}
 .wrapper {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
@@ -54,7 +108,7 @@
 }
 
 .box2n{
-  padding:9px;
+  padding:10px;
   display: grid;
   grid-template-columns: 2fr 1fr;
   grid-gap: 1px;
@@ -70,5 +124,7 @@
   display: grid;
   grid-template-columns: 10fr 3fr 3fr 10fr;
 }
+
+
 
 </style>
