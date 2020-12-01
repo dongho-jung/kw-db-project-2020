4e3c0199fb64eb12f99f43b5f24961c6705671f1
0xF4D3C0D3 <dongho971220@gmail.com>
2020-12-02 02:06:54 +0900

    adjust dummy data amount
    Signed-off-by: 0xF4D3C0D3 <dongho971220@gmail.com>

--- apps/dummy_data/models/preclass.py
+++ apps/dummy_data/models/preclass.py
@@ -6,30 +6,43 @@ class PreclassModel:
     def __init__(self, class_ids):
         self._class_ids = set(class_ids)
 
-        _l0 = set(random.sample(self._class_ids, 2))
+        _l0 = set(random.sample(self._class_ids, 4))
         self._class_ids -= _l0
 
-        _l1 = set(random.sample(self._class_ids, 2))
+        _l1 = set(random.sample(self._class_ids, 7))
         self._class_ids -= _l1
 
-        _l2 = set(random.sample(self._class_ids, 3))
+        _l2 = set(random.sample(self._class_ids, 6))
         self._class_ids -= _l2
 
-        _l3 = set(random.sample(self._class_ids, 2))
+        _l3 = set(random.sample(self._class_ids, 5))
         self._class_ids -= _l3
 
-        self._levels = [_l0, _l1, _l2, _l3]
+        _l4 = set(random.sample(self._class_ids, 6))
+        self._class_ids -= _l4
+
+        _l5 = set(random.sample(self._class_ids, 5))
+        self._class_ids -= _l5
+
+        self._levels = [_l0, _l1, _l2, _l3, _l4, _l5]
+        self._added_keys = set()
 
     def __iter__(self):
         return self
 
     def __next__(self):
         try:
-            level = random.choices([0, 1, 2, 3], map(len, self._levels), k=1)[0]
-            _class = random.sample(self._levels[level], 1)[0]
-            self._levels[level] -= {_class}
-            return (
-            "_" if level == 0 else random.sample(set(itertools.chain.from_iterable(self._levels[:level])), 1)[0],
-            _class)
+            while True:
+                level = random.choices([0, 1, 2, 3, 4, 5], k=1)[0]
+                _class = random.sample(self._levels[level], 1)[0]
+                key = (
+                    "_" if level == 0 else random.sample(set(itertools.chain.from_iterable(self._levels[:level])), 1)[
+                        0],
+                    _class)
+                if key[0] and key[1] and key not in self._added_keys:
+                    self._added_keys |= {key}
+                    if random.random() > 0.8:
+                        self._levels[level] -= {_class}
+                    return key
         except ValueError:
             raise StopIteration from None
