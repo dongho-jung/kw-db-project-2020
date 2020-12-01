29a6932a1e140db0edadca69d64f3cf6da38c53a
HoSungChoo <57245883+HoSungChoo@users.noreply.github.com>
2020-12-01 17:24:09 +0900

    Add files via upload

---
+++ src2/components/login/Login.vue
@@ -0,0 +1,386 @@
+<!-- 상단에 있는 로그 아웃 누르면 나오는 페이지 -->
+
+<template>
+    <div class="simpleLogin">
+        <form>
+            <img alt="DB1" src="@/assets/kw01.jpg" height="200" width="320">
+                <legend>
+                  <h1>DB MANMAN<br>Time Planner</h1>
+                </legend>
+
+            <div class="Lbox1">
+        <div class="LPad">
+        </div>
+
+        <div class="Lbox11">
+            <div class="LPad">
+            </div>
+
+            <div class="LBox">ID : 
+            </div>
+
+            <div class="Lbox">
+                <input id="name" placeholder="Username" type="text" style="width:270px;">
+            </div>
+
+            <div class="LPad">
+            </div>
+
+            <div class="Lbox">
+                <button id = "New Account" style="width:140px;" v-on:click="btn_NewAccount">New Account</button>
+            </div>
+        </div>
+
+        <div class="Lbox11">
+            <div class="LPad">
+            </div>
+
+            <div class="LBox">PW : 
+            </div>
+
+            <div class="Lbox">
+                <input id="pass" placeholder="Password" type="password" style="width:270px;">
+            </div>
+
+            <div class="LPad">
+            </div>
+
+            <div class="Lbox">
+                <button id = "Find PW" style="width:140px;" v-on:click="btn_FindPW">Find PW</button>
+            </div>
+        </div>
+
+        <div class="Lbox12" style="padding:20px;">
+            <div class="LPad">
+            </div>
+
+            <div class="Lbox">
+                <input type="button" value="Log In" style="width:140px; height:40px;"> 
+            </div>
+
+            <div class="LPad">
+            </div>
+
+            <div class="Lbox">
+                <a href="/login"><img src="../../assets/kakao.png" style= "width:140px; height:40px;"></a>
+            </div>
+
+            <div class="LPad">
+            </div>
+        </div>
+
+      </div>    
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Bbox33',
+        //메소드는 methods 객체 안에 정의
+        methods: {
+            btn_NewAccount: function () {
+                this
+                    .$router
+                    .push('/NewAccount')
+            },
+            btn_FindPW: function () {
+                this
+                    .$router
+                    .push('/FindPW')
+            }
+        }
+    }
+</script>
+
+<style>
+.simpleLogin form {
+    max-width: 400px;
+    margin: auto;
+    background: #fefefd;
+    box-shadow: 0 10px 10px #222;
+}
+.simpleLogin form fieldset {
+    display : grid;
+
+    border: 0 none;
+    margin: 0;
+    padding: 50px;
+}
+.simpleLogin form fieldset input,
+.simpleLogin form legend {
+    font-family: Open Sans;
+    font-size: 15px;
+}
+.simpleLogin form legend {
+    background-color: #8fc400;
+    border-top: 0 none;
+    color: white;
+    display: table-cell;
+    padding: 10px 20px;
+    width: auto;
+}
+.simpleLogin form fieldset input {
+    width: 90%;
+    margin: 10px 0;
+    padding: 10px 5%;
+    border: thin #8fc400 solid;
+}
+.simpleLogin input[type="submit"] {
+    width: 100px;
+    float: right;
+    background: #8fc400;
+    color: white;
+    transition: 0.2s;
+    border: 0;
+    cursor: pointer;
+}
+.simpleLogin input[type="submit"]:focus,
+.simpleLogin input[type="submit"]:hover,
+.simpleLogin input[type="submit"]:active {
+    padding: 10px 5%;
+    background: #B3E226;
+    outline: none;
+}
+.button {
+    color: black;
+    padding: 7px 60px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    cursor: pointer;
+}
+a {
+    text-decoration: none;
+    color: black;
+}
+.Login_part > div {
+    border-radius: 5px;
+    background-color: white;
+    padding: 1em;
+}
+.Login_part {
+    display: grid;
+    grid-template-rows: 400px 60px 60px 40px;
+    grid-gap: 10px;
+}
+.BPad {}
+.Bbox1 {}
+.Bbox2 {
+    display: grid;
+    grid-template-columns: 5fr 1fr 3fr 1fr 5fr;
+}
+.Bbox21 {}
+.Bbox22 {}
+.Bbox23 {}
+.Bbox3 {
+    display: grid;
+    grid-template-columns: 5fr 1fr 3fr 1fr 5fr;
+}
+.Bbox31 {}
+.Bbox32 {}
+.Bbox33 {}
+.Bbox4 {
+    display: grid;
+    grid-template-columns: 10fr 3fr 3fr 10fr;
+}
+
+.Bbox40 {
+    display: grid;
+    grid-template-columns: 1fr 10fr 1fr 4fr 1fr;
+    grid-template-rows: 2fr 1fr 2fr;
+    padding: 10px;
+}
+.Bbox41 {
+    display: grid;
+    grid-template-columns: 1fr 4fr 1fr 4fr 1fr;
+}
+.Bbox42 {}
+.Bbox43 {}
+.Bbox44 {}
+#footer,
+#header,
+#nav,
+#section {
+    text-align: center;
+}
+#footer,
+#header {
+    line-height: 100px;
+}
+#nav,
+#section {
+    line-height: 240px;
+}
+
+:root {}
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Alata', sans-serif;
+}
+
+.page-container {
+    width: 100vw;
+    height: 100vh;
+    background: #eff0f2;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+.shadow {
+    -webkit-box-shadow: 27px 43px 43px -26px rgba(89,89,89,0.39);
+    -moz-box-shadow: 27px 43px 43px -26px rgba(89,89,89,0.39);
+    box-shadow: 27px 43px 43px -26px rgba(89,89,89,0.39);
+}
+/*
+.shadow-light{
+-webkit-box-shadow: 45px 45px 104px -33px rgba(38,38,38,0.92);
+-moz-box-shadow: 45px 45px 104px -33px rgba(38,38,38,0.92);
+box-shadow: 45px 45px 104px -33px rgba(38,38,38,0.92);
+
+}*/
+.login-form-container {
+
+    background: #f5f5f5;
+    width: 860px;
+    height: 540px;
+    display: flex;
+    flex-direction: row;
+    box-shadow: 10px black;
+    border-radius: 10px;
+
+}
+.login-form-right-side {
+    width: 50%;
+    border-radius: 10px 0 0 10px;
+    padding: 75px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    background-image: radial-gradient(ellipse farthest-corner at 0 140%, #5d9dff 0%, #2178ff 70%, #3585ff 70%);
+}
+.login-form-right-side h1 {
+    color: white;
+    width: 100%;
+    text-align: right;
+    opacity: 0.9;
+
+}
+.login-form-right-side p {
+    padding-top: 50px;
+    font-size: 12px;
+    text-align: right;
+    opacity: 0.8;
+}
+.login-form-left-side {
+    width: 50%;
+    border-radius: 0 10px 10px 0;
+    display: flex;
+
+    flex-direction: column;
+    align-items: center;
+    padding: 40px;
+    background: rgb(255,255,255);
+    background: linear-gradient(287deg, rgba(255,255,255,1) 0%, rgba(243,244,244,1) 0%, rgba(255,255,255,1) 100%);
+}
+.login-form-left-side .login-top-wrap {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    width: 100%;
+}
+.login-form-left-side .login-top-wrap span {
+    color: gray;
+    font-size: 11px;
+    padding-right: 20px;
+
+}
+.login-form-left-side .login-top-wrap .create-account-btn {
+    background: white;
+    border: 0;
+    width: 85px;
+    height: 35px;
+    font-size: 11px;
+    color: #2178ff;
+    border-radius: 3px;
+
+}
+.login-input-container {
+    padding-top: 120px;
+    width: 300px;
+}
+.login-input-container .login-input-wrap {
+    width: 300px;
+    height: 45px;
+    margin-top: 20px;
+    border-radius: 2px;
+    border-bottom: solid 2px #2178ff;
+
+}
+.login-input-container .login-input-wrap i {
+    color: #2178ff;
+    line-height: 45px;
+}
+
+.login-input-container .login-input-wrap input {
+    background: none;
+
+    border: none;
+    line-height: 45px;
+    padding-left: 10px;
+    width: 267px;
+}
+.login-input-container .login-input-wrap input:focus {
+    outline: none;
+}
+.login-btn-wrap {
+    margin-top: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.login-btn-wrap .login-btn {
+    width: 95px;
+    height: 35px;
+    color: white;
+    border: 0;
+    border-radius: 4px;
+
+    background: rgb(105,163,255);
+    background: linear-gradient(162deg, rgba(105,163,255,1) 0%, rgba(43,125,254,1) 50%, rgba(43,125,254,1) 100%);
+}
+.login-btn-wrap a {
+    margin-top: 10px;
+    text-decoration: none;
+    font-size: 11px;
+    color: gray;
+
+}
+
+.Lbox1{
+display: grid;
+grid-template-columns: 800px;
+grid-template-rows: 100px 40px 80px 50px;
+}
+
+.Lbox11{
+    display: grid;
+    grid-template-columns: 100px 100px 250px 100px 100px;
+}
+
+.Lbox12{
+    display: grid;
+    grid-template-columns: 3fr 3fr 1fr 3fr 3fr;
+}
+
+.LPad{
+}
+
+.Lbox{
+}
+</style>
---
+++ src2/components/login/NewAccount.vue
@@ -0,0 +1,256 @@
+<template>
+  <div class="simpleLogin">
+    <form>
+        <div class="JBox" style="align:center;padding : 10px;">
+            <a href="http://localhost:3000/NewAccount">
+            <img alt="DB1" src="@/assets/NewAccount.png" height="200" width="270">
+            </a>
+        </div>
+      <legend>
+        <h1>New Account</h1>
+      </legend>
+      
+      <div class="Jbox1" >
+        <div class="JPad">
+        </div>
+
+        <div class="Jbox11">
+            <div class="JPad">
+            </div>
+
+            <div class="JBox">* Student ID : 
+            </div>
+
+            <div class="JBox">
+                <input v-model="student_id" type="text" name="student_id" size = 50 style = "text-align:center; ">
+            </div>
+
+            <div class="JBox">
+            </div>
+
+            <div class="JPad">
+            </div>
+        </div>
+
+        <div class="Jbox11">
+            <div class="JPad">
+            </div>
+
+            <div class="JBox">* Name : 
+            </div>
+
+            <div class="JBox">
+                <input v-model="kakao_name" type="text" name="name" size = 50 style = "text-align:center;">
+            </div>
+
+            <div class="JBox">
+            </div>
+
+            <div class="JPad">
+            </div>
+        </div>
+
+        <div class="Jbox11">
+            <div class="JPad">
+            </div>
+
+            <div class="JBox">Phone :
+            </div>
+
+            <div class="JBox">
+                <input type="text" name="phone" size = 50 style = "text-align:center;">
+            </div>
+
+            <div class="JBox">
+            </div>
+
+            <div class="JPad">
+            </div>
+        </div>
+
+        <div class="Jbox11">
+            <div class="JPad">
+            </div>
+
+            <div class="JBox">Gender : 
+            </div>
+
+            <div class="JBox">
+                <input v-model="kakao_gender" type="text" name="gender" size = 50 style = "text-align:center;">
+            </div>
+
+            <div class="JBox">
+            </div>
+
+            <div class="JPad">
+            </div>
+        </div>
+
+        <div class="Jbox11">
+            <div class="JPad">
+            </div>
+
+            <div class="JBox">* Email : 
+            </div>
+
+            <div class="JBox">
+                <input v-model="kakao_email" type="text" name="email" size = 50 style = "text-align:center;">
+            </div>
+
+            <div class="JBox">
+                <input v-on:click="Check_email" type="button" value="중복체크" size = 50 style = "width:80pt;height:16pt;text-align:center;">
+            </div>
+
+            <div class="JPad">
+            </div>
+        </div>
+
+        <div class="Jbox11">
+            <div class="JPad">
+            </div>
+
+            <div class="JBox">Birth(YYMMDD) :
+            </div>
+
+            <div class="JBox">
+                <input type="text" v-model="kakao_birth" name="birth" size = 50 style = "text-align:center;">
+            </div>
+
+            <div class="JBox">
+            </div>
+
+            <div class="JPad">
+            </div>
+        </div>
+
+        <div class="Jbox11">
+            <div class="JPad">
+            </div>
+
+            <div class="JBox">* PW :
+            </div>
+
+            <div class="JBox">
+                <input type="text" name="PW" size = 50 maxlength=10>
+            </div>
+
+            <div class="JBox">
+            </div>
+
+            <div class="JPad">
+            </div>
+        </div>
+
+        <div class="Jbox12" style="padding:20px;">
+            <div class="JPad">
+            </div>
+
+            <div class="JBox">
+                <input type="button" v-on:click="OK" value="OK" size=70 style = "width:100pt;height:20pt;text-align:center;">
+            </div>
+
+            <div class="JBox">
+                <input type="button" v-on:click="Cancel" value="Cancel" size=70 style = "width:100pt;height:20pt;text-align:center;">
+            </div>
+
+            <div class="JBox">
+                <p v-on:click="KakaoLogin" ><a href="https://kauth.kakao.com/oauth/authorize?client_id=8d323fc0c13720cda59983912f875316&redirect_uri=http://localhost:5000/oauth&response_type=code">
+                <img src="../../assets/kakaoNewAccount.png" height="30"></a></p>
+            </div>
+
+            <div class="JPad">
+            </div>
+        </div>
+
+      </div>
+    
+    </form>
+  </div>
+</template>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.simpleLogin form {
+  min-width: 760px;
+  min-height: 650px;
+  margin: auto;
+  background: #fefefd;
+  box-shadow: 0 10px 10px #222;
+}
+.simpleLogin form fieldset {
+  display : grid;
+  margin: 0;
+  padding: 50px;
+}
+.simpleLogin form fieldset input,
+.simpleLogin form legend {
+  font-family: Open Sans;
+  font-size: 15px;
+}
+.simpleLogin form legend {
+  background-color: #8fc400;
+  border-top: 0 none;
+  color: white;
+  display: table-cell;
+  padding: 10px 20px;
+  width: auto;
+}
+.simpleLogin form fieldset input {
+  width: 90%;
+  margin: 10px 0;
+  padding: 10px 5%;
+  border: thin #8fc400 solid;
+}
+.simpleLogin input[type="submit"] {
+  width: 100px;
+  float: right;
+  background: #8fc400;
+  color: white;
+  transition: 0.2s;
+  border: 0;
+  cursor: pointer;
+}
+.simpleLogin input[type="submit"]:focus,
+.simpleLogin input[type="submit"]:hover,
+.simpleLogin input[type="submit"]:active {
+  padding: 10px 5%;
+  background: #B3E226;
+  outline: none;
+}
+.button {
+  color: black;
+  padding: 7px 60px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  cursor: pointer;
+  margin: 4px 2px;
+}
+
+#header, #nav, #section, #footer { text-align:center; }
+#header, #footer { line-height:100px; }
+#nav, #section { line-height:240px; }
+
+.Jbox1{
+    display: grid;
+    grid-template-columns: 800px;
+    grid-template-rows: 20px 40px 40px 40px 40px 40px 40px 40px 50px;
+}
+
+.Jbox11{
+    display: grid;
+    grid-template-columns: 50px 150px 400px 150px 50px;
+}
+
+.Jbox12{
+    display: grid;
+    grid-template-columns: 2fr 3fr 3fr 3fr 2fr;
+}
+
+.JPad{
+}
+
+.JBox{
+}
+
+
+</style>
